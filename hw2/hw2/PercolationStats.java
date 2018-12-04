85b12ad09e7e1fde2cc969e914c6667b4d2830d1
Yingduo Chen <yingduo.chen@gmail.com>
2018-12-03 23:47:46 -0800

    remove unrelated input and output files

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -2,4 +2,36 @@ package hw2;
 
 public class PercolationStats {
 
+    /**
+     * perform T independent experiments on an N-by-N grid
+     * @param N
+     * @param T
+     * @param pf
+     */
+    public PercolationStats(int N, int T, PercolationFactory pf){
+        if( N <= 0 || T <= 0) throw new IllegalArgumentException("input N or T is not positive");
+        int i = 0;
+        while (i < T) {
+            pf.make(N);
+            i++;
+        }
+
+    }
+
+    public double mean() {
+        return -1;
+    }
+
+    public double stdev() {
+        return -1;
+    }
+
+    public double confidenceLow() {
+        return -1;
+    }
+
+    public double confidenceHigh() {
+        return -1;
+    }
+
 }
